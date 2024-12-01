fc0f178c7917dbddbdc12a66bc2ad9f2ecae0ef6
HajayL <84947010+HajayL@users.noreply.github.com>
2024-12-01 13:09:28 -0700

    Added sorting

--- Day_1/Program.cs
+++ Day_1/Program.cs
@@ -8,8 +8,44 @@ namespace Day_1
 {
     internal class Program
     {
+
+        static Random rnd = new Random();
+        
+
         static void Main(string[] args)
         {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            int result;
+
+            Console.ReadKey();
+        }
+
+        static void SwapItem(List<int> list, int indexA, int indexB)
+        {
+            int B = list[indexB];
+            list[indexB] = list[indexA];
+            list[indexA] = B;
+        }
+
+        static void Sort(List<int> list)
+        {
+            bool listChanged;
+
+            do
+            {
+                listChanged = false;
+
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        listChanged = true;
+                        SwapItem(list, i, i + 1);
+                    }
+                }
+
+            } while (listChanged);
         }
     }
 }
